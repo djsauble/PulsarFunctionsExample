0970ed45e96b0cc2c60a122fdce98cb1fb8cae17
Daniel Sauble <djsauble@gmail.com>
2021-11-29 15:15:36 -0800

    Clean up

--- src/main/java/com/datastax/PredictLabelsFunction.java
+++ src/main/java/com/datastax/PredictLabelsFunction.java
@@ -6,6 +6,6 @@ import org.apache.pulsar.functions.api.Function;
 public class PredictLabelsFunction implements Function<String, String> {
         @Override
         public String process(String input, Context ctx) throws Exception {
-                return String.format("%s!", input);
+                return String.format("!!!%s!!!", input);
         }
 }
