fb48c4e671d090e32208958f3cee5463c89da607
Daniel Sauble <djsauble@gmail.com>
2021-11-30 21:36:28 -0800

    Cleaned up regression example

---
+++ src/main/java/RegressionFunction.java
@@ -0,0 +1,62 @@
+import java.io.ByteArrayInputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.List;
+import java.nio.ByteBuffer;
+
+import org.apache.pulsar.functions.api.Context;
+import org.apache.pulsar.functions.api.Function;
+
+import org.dmg.pmml.FieldName;
+import org.jpmml.evaluator.ModelEvaluatorBuilder;
+import org.jpmml.evaluator.Evaluator;
+import org.jpmml.evaluator.EvaluatorBuilder;
+import org.jpmml.evaluator.FieldValue;
+import org.jpmml.evaluator.InputField;
+import org.jpmml.evaluator.TargetField;
+import org.jpmml.model.PMMLUtil;
+
+public class RegressionFunction implements Function<String, String> {
+
+        private static final String MODEL_KEY = "ML-Model";
+
+        @Override
+        public String process(String input, Context ctx) throws Exception {
+
+                ByteBuffer state = ctx.getState(MODEL_KEY);
+
+                if (state == null) {
+                        return "No model has been uploaded";
+                }
+                else {
+                        byte[] mlModel = state.array();
+                        ByteArrayInputStream stream = new ByteArrayInputStream(mlModel);
+                        try {
+                                // Build and verify the model
+                                EvaluatorBuilder builder = new ModelEvaluatorBuilder(PMMLUtil.unmarshal(stream));
+                                Evaluator evaluator = builder.build();
+                                evaluator.verify();
+
+                                // Get a list of features and target variables from the model
+                                List<? extends InputField> inputFields = evaluator.getInputFields();
+                                List<? extends TargetField> targetFields = evaluator.getTargetFields();
+
+                                // Construct the input to the model
+                                Map<FieldName, FieldValue> featureVector = new LinkedHashMap<>();
+                                InputField inputField = inputFields.get(0);
+                                featureVector.put(inputField.getName(), inputField.prepare(Double.valueOf(input)));
+
+                                // Evaluate the model on the inputs
+                                Map<FieldName, ?> results = evaluator.evaluate(featureVector);
+
+                                // Extract the prediction
+                                FieldName targetField = targetFields.get(0).getName();
+                                return Double.toString((Double)results.get(targetField));
+
+                        }
+                        catch (Exception exception) {
+                                return exception.getMessage();
+                        }
+                }
+        }
+}
